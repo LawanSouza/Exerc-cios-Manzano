1fe160bd48cb82cc9b17ad1fbf372d58f2aee035
LawanSouza <146399438+LawanSouza@users.noreply.github.com>
2023-11-21 21:50:02 -0300

    Exercicio02.java

--- Exercicio02.java
+++ Exercicio02.java
@@ -1,17 +1,15 @@
 package exerciciosManzano;
-
 public class Exercicio02 {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) 
+	{
 		
 		int soma = 0;
-
-        for (int i = 1; i <= 100; i++) {
-            soma += i;
-        }
-
-        System.out.println("Total da soma dos cem primeiros números inteiros: " + soma);
-
+		for (int i = 1; i <= 100; i++)
+			
+		{
+			soma += i;
+		}
+		System.out.println("Total da soma dos cem primeiros números inteiros: " + soma);
 	}
-
 }
